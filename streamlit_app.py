859cb0023d329abfcd256deb27a67b39c763a1fb
Daniel Sz <danielsz@Daniels-MacBook-Air.local>
2025-04-10 19:45:37 +0200

    changing title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 # Streamlit Page Configuration
 st.set_page_config(
-    page_title="Streamly - An Intelligent Streamlit Assistant",
+    page_title="DocAgent AI",
     page_icon="images/logo.png",
     layout="wide",
     initial_sidebar_state="auto",
@@ -17,7 +17,7 @@ st.set_page_config(
 )
 
 # Streamlit Title
-st.title("DocAgent AI")
+# st.title("DocAgent AI")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
